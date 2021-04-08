93e18de6e0f100aaba5d55c0f9d1f4158321a902
Kevin Rangel <code@venikx.com>
2021-04-08 15:12:29 +0300

    Initialize npm package

---
+++ shell.nix
@@ -0,0 +1,31 @@
+/**
+* Nix is used as an alternative to popular package managers of linux
+* distributions, or as an alternative to Homebrew.
+*
+* The project does NOT depend on Nix or NixOS and `shell.nix` is
+* effectively ignored if the system you are running does NOT use Nix
+* or NixOS.
+* Anyone who uses Nix as a package manager is able to run
+* `shell.nix` to set up the project specific development environment
+* without polluting the overall system with project specific
+* packages.
+*
+* A couple links if you are interested in learning more about Nix:
+* - https://nixos.org/guides/dev-environment.html
+* - https://github.com/uniphil/nix-for-devs
+* - https://wickedchicken.github.io/post/macos-nix-setup/
+*/
+
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    nodejs-14_x
+    nodePackages.pnpm
+  ];
+  shellHook = ''
+    export PATH="$PWD/node_modules/.bin/:$PATH"
+  '';
+}
